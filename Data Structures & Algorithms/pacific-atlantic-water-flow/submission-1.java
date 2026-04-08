b8bff98208b17441602f8003eb4cc408445f1647
Yatik <110741464+yatiksihag01@users.noreply.github.com>
2026-04-09 01:25:05 +0530

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.java
@@ -0,0 +1,48 @@
+class Solution {
+    private static final int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int r = 0; r < m; r++) {
+            dfs(heights, r, 0, pacific, heights[r][0]);
+            dfs(heights, r, n - 1, atlantic, heights[r][n - 1]);
+        }
+
+        for (int c = 0; c < n; c++) {
+            dfs(heights, 0, c, pacific, heights[0][c]);
+            dfs(heights, m - 1, c, atlantic, heights[m - 1][c]);
+        }
+
+        List<List<Integer>> result = new ArrayList();
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (pacific[r][c] && atlantic[r][c]) {
+                    result.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int[][] heights, int r, int c, boolean[][] visited, int prevHeight) {
+        int m = heights.length;
+        int n = heights[0].length;
+
+        if (r < 0 || c < 0 ||
+            r >= m || c >= n ||
+            visited[r][c] ||
+            heights[r][c] < prevHeight) {
+            return;
+        }
+
+        visited[r][c] = true;
+        for (int[] dir : DIRECTIONS) {
+            dfs(heights, r + dir[0], c + dir[1], visited, heights[r][c]);
+        }
+    }
+}
