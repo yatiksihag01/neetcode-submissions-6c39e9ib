966962a992552d417ddc339d54efd85c503f0505
Yatik <110741464+yatiksihag01@users.noreply.github.com>
2026-05-19 23:18:11 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+        int size = nums.length;
+        if (size == 1) return nums[0];
+        return Math.max(
+            robHelper(nums, 0, size - 1),
+            robHelper(nums, 1, size)
+        );
+    }
+
+    private int robHelper(int[] nums, int startFrom, int endsBefore) {
+        int size = nums.length;
+        int prevMax = 0;
+        int currMax = 0;
+
+        for (int i = startFrom; i < endsBefore; i++) {
+            int nextMax = Math.max(currMax, prevMax + nums[i]);
+            prevMax = currMax;
+            currMax = nextMax;
+        }
+
+        return currMax;
+    }
+}
